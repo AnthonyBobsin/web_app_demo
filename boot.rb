2b1fa06ea81648165c93ce9f80cf9740eeab4d7b
Anthony Bobsin <abobsin@adparlor.com>
2016-08-30 20:37:48 -0400

    Adds environment variable specifiers that allow us to use different database/configuration based on the environment (in this case APP_ENV) that the server is started with

--- boot.rb
+++ boot.rb
@@ -1,4 +1,10 @@
 
+ENV['APP_ENV'] = 'development' unless ENV['APP_ENV']
+
+# allows `rake db:create` usage
+ENV['RAILS_ENV'] = ENV['APP_ENV']
+ENV['RACK_ENV' ] = ENV['APP_ENV']
+
 require 'rubygems'
 require 'bundler'
 
